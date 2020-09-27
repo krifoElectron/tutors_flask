ff9c21f19cac408e80317c61426eb7e71e6c95b4
Gordeev Arsenii <Gordeev.A.Vladim@sberbank.ru>
2020-09-27 21:46:17 +0300

    часть ДЗ 4

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import request
 from functools import reduce
 import json
 from random import randint
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from forms import BookingForm
 from forms import RequestForm
 
@@ -22,40 +24,92 @@ def save_data_to_db(new_db):
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'my-super-secret-phrase-I-dont-tell-this-to-nobody'
+
+
+class Tutor(db.Model):
+    __tablename__ = "tutors"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    about = db.Column(db.String(200), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    picture = db.Column(db.String(256))
+    price = db.Column(db.Integer, nullable=False)
+    goals = db.Column(db.String(200), nullable=False)  # в виде строки будет json, который преобразуется в список
+    free = db.Column(db.String(), nullable=False)  # в виде строки будет json, который преобразуется в словарь
+    bookings = db.relationship("Booking")  # у репетитора может быть несколько бронирований
+
+    def get_dict(self):
+        return {
+            'name': self.name,
+            'about': self.about,
+            'rating': self.rating,
+            'picture': self.picture,
+            'price': self.price,
+            'goals': self.goals,
+            'free': self.free,
+            'bookings': self.bookings,
+        }
+
+
+class Booking(db.Model):
+    __tablename__ = "bookings"
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(20), nullable=False)
+    teacher_id = db.Column(db.Integer, db.ForeignKey("tutors.id"))  # связь с репетитором
+    teacher = db.relationship("Tutor")
+    day_of_week = db.Column(db.String(3), nullable=False)  # mon,...
+    time = db.Column(db.String(2), nullable=False)  # 16
+    name = db.Column(db.String(30), nullable=False)
+    phone = db.Column(db.String(12), nullable=False)
+
+
+class Request(db.Model):
+    __tablename__ = "requests"
+    id = db.Column(db.Integer, primary_key=True)
+    goal = db.Column(db.String(20), nullable=False)
+    time = db.Column(db.String(4), nullable=False)  # 7-10
+    name = db.Column(db.String(30), nullable=False)
+    phone = db.Column(db.String(12), nullable=False)
 
 
 @app.route('/')
 def render_main():
-    db = get_data_from_db()
-    profiles = db['teachers']
-    goals = db['goals']
+    data_base = get_data_from_db()
+    tutors = db.session.query(Tutor).all()  # Использование БД
+    print(type(tutors))
+    goals = data_base['goals']
 
-    all_numbers = [i for i in range(len(profiles))]
+    all_numbers = [i for i in range(len(tutors))]
     random_numbers = []
     for i in range(6):
         random_numbers.append(all_numbers.pop(randint(0, len(all_numbers) - 1)))
 
     random_profiles = []
     for random_number in random_numbers:
-        random_profiles.append(profiles[random_number])
+        random_profiles.append(tutors[random_number])
 
     return render_template('index.html', profiles=random_profiles, goals=goals)
 
 
 @app.route('/all_tutors')
 def render_all_tutors():
-    db = get_data_from_db()
-    profiles = db['teachers']
-    goals = db['goals']
-    return render_template('all_tutors.html', profiles=profiles, goals=goals)
+    tutors = db.session.query(Tutor).all()  # Использование БД
+
+    data_base = get_data_from_db()
+    goals = data_base['goals']
+    return render_template('all_tutors.html', profiles=tutors, goals=goals)
 
 
 @app.route('/profiles/<int:teacher_id>')
 def render_profile(teacher_id):
-    db = get_data_from_db()
-    teachers = db['teachers']
-    goals = db['goals']
+    data_base = get_data_from_db()
+    teachers = data_base['teachers']
+    goals = data_base['goals']
 
     profile = [teacher for teacher in teachers if teacher['id'] == teacher_id][0]
     profile_goals = [value for key, value in goals.items() if key in profile['goals']]
@@ -73,8 +127,8 @@ def render_profile(teacher_id):
 
 @app.route('/booking/<int:teacher_id>/<string:day_of_week>/<int:time>')
 def render_booking(teacher_id, day_of_week, time):
-    db = get_data_from_db()
-    teacher = db['teachers'][teacher_id]
+    data_base = get_data_from_db()
+    teacher = data_base['teachers'][teacher_id]
     form = BookingForm(client_weekday=day_of_week, client_time=time, client_teacher_id=teacher['id'])
 
     return render_template('booking.html', teacher=teacher, map_to_day_of_week=map_to_day_of_week,
@@ -97,9 +151,9 @@ def render_booking_done():
         'phone': client_phone
     }
 
-    db = get_data_from_db()
-    db['bookings'].append(booking)
-    save_data_to_db(db)
+    data_base = get_data_from_db()
+    data_base['bookings'].append(booking)
+    save_data_to_db(data_base)
 
     return render_template('booking_done.html', client_weekday=client_weekday, client_time=client_time,
                            client_name=client_name, client_phone=client_phone)
@@ -125,9 +179,9 @@ def render_request_done():
         'phone': request_phone
     }
 
-    db = get_data_from_db()
-    db['requests'].append(client_request)
-    save_data_to_db(db)
+    data_base = get_data_from_db()
+    data_base['requests'].append(client_request)
+    save_data_to_db(data_base)
 
     return render_template('request_done.html', request_goal=request_goal,
                            request_time=request_time,
@@ -137,10 +191,10 @@ def render_request_done():
 
 @app.route('/goals/<string:goal>')
 def render_goal(goal):
-    db = get_data_from_db()
-    teachers = db['teachers']
+    data_base = get_data_from_db()
+    teachers = data_base['teachers']
     filtered_teachers = list(filter(lambda x: goal in x['goals'], teachers))
-    goals = db['goals']
+    goals = data_base['goals']
     return render_template('goal.html', goal=goals[goal], teachers=filtered_teachers)
 
 
