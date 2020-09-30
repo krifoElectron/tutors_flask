de424064ee4ce25ebc3bfe6aabce57e92adc2764
Gordeev Arsenii <Gordeev.A.Vladim@sberbank.ru>
2020-10-01 01:28:48 +0300

    4 ДЗ целиком

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
+import json
+from functools import reduce
+
 from flask import Flask
 from flask import render_template
 from flask import request
-from functools import reduce
-import json
-from random import randint
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+
 from forms import BookingForm
 from forms import RequestForm
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 map_to_day_of_week = {'mon': 'Понедельник', 'tue': 'Вторник', 'wed': 'Среда', 'thu': 'Четверг',
                       'fri': 'Пятница', 'sat': 'Суббота', 'sun': 'Воскресение'}
-
-
-def get_data_from_db():
-    with open('db.json', 'r') as f:
-        return json.load(f)
-
-
-def save_data_to_db(new_db):
-    with open('db.json', 'w') as f:
-        return json.dump(new_db, f, indent=4)
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -30,18 +21,24 @@ migrate = Migrate(app, db)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'my-super-secret-phrase-I-dont-tell-this-to-nobody'
 
+tutors_goals_association = db.Table(
+    'users_chats',
+    db.Column('tutor_id', db.Integer, db.ForeignKey('tutors.id', primary_key=True)),
+    db.Column('goal_id', db.Integer, db.ForeignKey('goals.id', primary_key=True))
+)
+
 
 class Tutor(db.Model):
-    __tablename__ = "tutors"
+    __tablename__ = 'tutors'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     about = db.Column(db.String(200), nullable=False)
     rating = db.Column(db.Float, nullable=False)
     picture = db.Column(db.String(256))
     price = db.Column(db.Integer, nullable=False)
-    goals = db.Column(db.String(200), nullable=False)  # в виде строки будет json, который преобразуется в список
     free = db.Column(db.String(), nullable=False)  # в виде строки будет json, который преобразуется в словарь
     bookings = db.relationship("Booking")  # у репетитора может быть несколько бронирований
+    goals = db.relationship('Goal', secondary=tutors_goals_association, back_populates='tutors')
 
     def get_dict(self):
         return {
@@ -56,10 +53,18 @@ class Tutor(db.Model):
         }
 
 
-class Booking(db.Model):
-    __tablename__ = "bookings"
+class Goal(db.Model):
+    __tablename__ = 'goals'
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+    display_name = db.Column(db.String(30), nullable=False)
+    tutors = db.relationship('Tutor', secondary=tutors_goals_association, back_populates='goals')
+
+
+class Booking(db.Model):
+    __tablename__ = 'bookings'
+    id = db.Column(db.Integer, primary_key=True)
+    booking_type = db.Column(db.String(20), nullable=False)
     teacher_id = db.Column(db.Integer, db.ForeignKey("tutors.id"))  # связь с репетитором
     teacher = db.relationship("Tutor")
     day_of_week = db.Column(db.String(3), nullable=False)  # mon,...
@@ -79,124 +84,103 @@ class Request(db.Model):
 
 @app.route('/')
 def render_main():
-    data_base = get_data_from_db()
-    tutors = db.session.query(Tutor).all()  # Использование БД
-    goals = data_base['goals']
-    all_numbers = [i for i in range(len(tutors))]
-    random_numbers = []
-    for i in range(6):
-        random_numbers.append(all_numbers.pop(randint(0, len(all_numbers) - 1)))
-
-    random_profiles = []
-    for random_number in random_numbers:
-        random_profiles.append(tutors[random_number])
+    goals = db.session.query(Goal).all()
+    random_profiles = Tutor.query.order_by(db.func.random()).limit(6)
 
     return render_template('index.html', profiles=random_profiles, goals=goals)
 
 
 @app.route('/all_tutors')
 def render_all_tutors():
-    tutors = db.session.query(Tutor).all()  # Использование БД
-
-    data_base = get_data_from_db()
-    goals = data_base['goals']
+    tutors = db.session.query(Tutor).all()
+    goals = db.session.query(Goal).all()
     return render_template('all_tutors.html', profiles=tutors, goals=goals)
 
 
 @app.route('/profiles/<int:teacher_id>')
 def render_profile(teacher_id):
-    data_base = get_data_from_db()
-    teachers = data_base['teachers']
-    goals = data_base['goals']
+    profile = db.session.query(Tutor).get(teacher_id)
+    if profile is None:
+        return f'Преподавателья с id = {teacher_id} не существует', 404
 
-    profile = [teacher for teacher in teachers if teacher['id'] == teacher_id][0]
-    profile_goals = [value for key, value in goals.items() if key in profile['goals']]
+    profile_goals = profile.goals
 
-    day_dict = [(day, day_obj) if
-                reduce(lambda x, y: x or y, day_obj.values(), False) else (day, None) for
-                day, day_obj in profile['free'].items()]
+    list_of_tuples_day_and_dict_value = [(day, day_obj) if
+                                         reduce(lambda x, y: x or y, day_obj.values(), False) else (day, None) for
+                                         day, day_obj in json.loads(profile.free).items()]
 
-    for (day, value) in day_dict:
-        profile['free'][day] = value
+    profile_free = {}
+    for day, value in list_of_tuples_day_and_dict_value:
+        profile_free[day] = value
 
-    return render_template('profile.html', profile=profile, profile_goals=profile_goals,
+    return render_template('profile.html', profile=profile, profile_free=profile_free, profile_goals=profile_goals,
                            map_to_day_of_week=map_to_day_of_week)
 
 
-@app.route('/booking/<int:teacher_id>/<string:day_of_week>/<int:time>')
+@app.route('/booking/<int:teacher_id>/<string:day_of_week>/<int:time>', methods=['GET', 'POST'])
 def render_booking(teacher_id, day_of_week, time):
-    data_base = get_data_from_db()
-    teacher = data_base['teachers'][teacher_id]
-    form = BookingForm(client_weekday=day_of_week, client_time=time, client_teacher_id=teacher['id'])
+    if request.method == 'POST':
+        client_weekday = request.form.get('client_weekday')
+        client_time = request.form.get('client_time')
+        client_name = request.form.get('client_name')
+        client_phone = request.form.get('client_phone')
+        client_teacher_id = request.form.get('client_teacher_id')
 
-    return render_template('booking.html', teacher=teacher, map_to_day_of_week=map_to_day_of_week,
-                           day_of_week=day_of_week, time=time, form=form)
+        booking_entity = Booking(
+            booking_type='trial_lesson',
+            teacher=db.session.query(Tutor).get(client_teacher_id),
+            day_of_week=client_weekday,
+            time=client_time,
+            name=client_name,
+            phone=client_phone)
 
+        db.session.add(booking_entity)
+        db.session.commit()
 
-@app.route('/booking_done/', methods=['POST'])
-def render_booking_done():
-    client_weekday = request.form.get('client_weekday')
-    client_time = request.form.get('client_time')
-    client_name = request.form.get('client_name')
-    client_phone = request.form.get('client_phone')
-    client_teacher_id = request.form.get('client_teacher_id')
-    booking = {
-        'type': 'trial_lesson',
-        'teacher_id': client_teacher_id,
-        'day_of_week': client_weekday,
-        'time': client_time,
-        'name': client_name,
-        'phone': client_phone
-    }
+        return render_template('booking_done.html', client_weekday=client_weekday, client_time=client_time,
+                               client_name=client_name, client_phone=client_phone)
 
-    data_base = get_data_from_db()
-    data_base['bookings'].append(booking)
-    save_data_to_db(data_base)
+    if request.method == 'GET':
+        tutor = db.session.query(Tutor).get(teacher_id)
+        form = BookingForm(client_weekday=day_of_week, client_time=time, client_teacher_id=tutor.id)
 
-    return render_template('booking_done.html', client_weekday=client_weekday, client_time=client_time,
-                           client_name=client_name, client_phone=client_phone)
+        return render_template('booking.html', teacher=tutor, map_to_day_of_week=map_to_day_of_week,
+                               day_of_week=day_of_week, time=time, form=form)
 
 
-@app.route('/request')
+@app.route('/request', methods=['GET', 'POST'])
 def render_request():
-    form = RequestForm()
-    return render_template('request.html', form=form)
+    if request.method == 'GET':
+        form = RequestForm()
+        return render_template('request.html', form=form)
+    elif request.method == 'POST':
+        request_goal = request.form.get('request_goal')
+        request_time = request.form.get('request_time')
+        request_name = request.form.get('request_name')
+        request_phone = request.form.get('request_phone')
 
+        request_entity = Request(
+            goal=request_goal,
+            time=request_time,
+            name=request_name,
+            phone=request_phone)
 
-@app.route('/request_done', methods=['POST'])
-def render_request_done():
-    request_goal = request.form.get('request_goal')
-    request_time = request.form.get('request_time')
-    request_name = request.form.get('request_name')
-    request_phone = request.form.get('request_phone')
+        db.session.add(request_entity)
+        db.session.commit()
 
-    client_request = {
-        'goal': request_goal,
-        'time': request_time,
-        'name': request_name,
-        'phone': request_phone
-    }
-
-    data_base = get_data_from_db()
-    data_base['requests'].append(client_request)
-    save_data_to_db(data_base)
-
-    return render_template('request_done.html', request_goal=request_goal,
-                           request_time=request_time,
-                           request_name=request_name,
-                           request_phone=request_phone)
+        return render_template('request_done.html',
+                               request_goal=request_goal,
+                               request_time=request_time,
+                               request_name=request_name,
+                               request_phone=request_phone)
 
 
 @app.route('/goals/<string:goal>')
 def render_goal(goal):
-    data_base = get_data_from_db()
-    goals = data_base['goals']
-    teachers = data_base['teachers']
-    filtered_teachers_2 = list(filter(lambda x: goal in x['goals'], teachers))
-    filtered_teachers = tutors = db.session.query(Tutor).filter(goal in Tutor.goals)  # Использование БД
-    print(f'{filtered_teachers}\n{filtered_teachers_2}')
-    print(filtered_teachers == filtered_teachers_2)
-    return render_template('goal.html', goal=goals[goal], teachers=filtered_teachers)
+    goal_entity = db.session.query(Goal).filter(Goal.name == goal).first()
+    goal_display_name = goal_entity.display_name
+    tutors = goal_entity.tutors
+    return render_template('goal.html', goal_display_name=goal_display_name, teachers=tutors)
 
 
 if __name__ == '__main__':
---
+++ seed_db.py
@@ -0,0 +1,30 @@
+import data
+import json
+from app import db, Tutor, Goal
+
+
+def seed_db():
+    goal_entities_dict = {}
+    for goal_name, goal_display_name in data.goals.items():
+        goal_entity = Goal(name=goal_name, display_name=goal_display_name)
+        db.session.add(goal_entity)
+        goal_entities_dict[goal_name] = goal_entity
+
+    for tutor in data.teachers:
+        tutor_entity = Tutor(name=tutor['name'],
+                             about=tutor['about'],
+                             rating=tutor['rating'],
+                             picture=tutor['picture'],
+                             price=tutor['price'],
+                             free=json.dumps(tutor['free']))
+
+        for goal in tutor['goals']:
+            tutor_entity.goals.append(goal_entities_dict[goal])
+
+        db.session.add(tutor_entity)
+
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    seed_db()
